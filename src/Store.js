bf8feeba363cdb0a451082a63efa4850fb375b5f
stoneyangxu <stoneyangxu@icloud.com>
2017-11-09 00:07:25 +0800

    add redux dev tools

--- src/Store.js
+++ src/Store.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import { reducer as todoReducer } from './todos'
 import { reducer as filterReducer } from './filter'
 
@@ -6,4 +6,17 @@ const reducer = combineReducers({
   todos: todoReducer,
   filter: filterReducer
 })
-export default createStore(reducer)
+
+const middlewires = []
+if (process.env.NODE_ENV !== 'production') {
+  middlewires.push(require('redux-immutable-state-invariant').default())
+}
+
+const win = window
+
+const storeEnhancers = compose(
+  applyMiddleware(...middlewires),
+  (win && win.devToolsExtension) ? win.devToolsExtension() : (f) => f,
+)
+
+export default createStore(reducer, {}, storeEnhancers)
