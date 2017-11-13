4642c64bf8ab2300451862d397d1650a81d8c469
stoneyangxu <stoneyangxu@icloud.com>
2017-11-14 01:39:58 +0800

    transition

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Filter } from './filter';
 import CountDown from './CountDown'
 import { Weather } from './weather'
 
+import { Motion, spring } from 'react-motion'
+
 class App extends Component {
   render() {
     return (
@@ -15,6 +17,14 @@ class App extends Component {
         <CountDown count={10}>
           {(count) => <div>{count}</div>}
         </CountDown>
+
+        <Motion defaultStyle={{ x: 100 }}
+          style={
+            { x: spring(0, { stiffness: 100, damping: 100 }) }
+          }
+        >
+          {value => <div>{Math.ceil(value.x)}</div>}
+        </Motion>
 
         <Weather />
       </div>
