15d6057d7547bbdc4ded84ce32efa3b2be2df49d
stoneyangxu <stoneyangxu@icloud.com>
2017-11-14 00:35:01 +0800

    using TransitionGroup

--- src/todos/views/todoList.js
+++ src/todos/views/todoList.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import TransitionGroup from 'react-addons-css-transition-group'
+
 import TodoItem from './todoItem'
 import { bindActionCreators } from 'redux'
 import { filterTypes as FilterTypes } from '../../filter'
 
-import {filterByCompleteState} from '../selector'
+import { filterByCompleteState } from '../selector'
+
+import './todoList.css'
 
 const TodoList = ({ todos, onToggle, onRemove }) => {
   return (
     <ul className="row list-group">
-      {
-        todos.map(todo => (
-          <TodoItem
-            id={todo.id}
-            key={todo.id}
-            text={todo.text}
-            completed={todo.completed}
-          />
-        ))
-      }
+      <TransitionGroup transitionName="fade" transitionEnterTimeout={500}
+        transitionLeaveTimeout={200} >
+
+        {
+          todos.map(todo => (
+            <TodoItem
+              id={todo.id}
+              key={todo.id}
+              text={todo.text}
+              completed={todo.completed}
+            />
+          ))
+        }
+      </TransitionGroup>
     </ul>
   )
 }
